5c68744b2f3b592ca848bb5955cd72dd6ee0b0dc
Ceci007 <cecibenitezca@gmail.com>
2020-06-20 14:25:48 -0500

    adding body description

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_142512) do
+ActiveRecord::Schema.define(version: 2020_06_20_153843) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
